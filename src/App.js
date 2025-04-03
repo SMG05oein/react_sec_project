7deaa6b27bd670b75c2a862cfd5c4f636f16942b
SMG <SMG05eoin@gmail.com>
2025-04-03 15:12:59 +0900

    현재 위치 확인

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const getCurrentLocation = () => {
+    // console.log("getCurrentLocation");
+    navigator.geolocation.getCurrentPosition((position) => { //위도 경도 받기
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      console.log("현재 위치: ", lat, lon);
+    });
+
+  }
+  
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+
   return (
-    <div className="App">
-      
+    <div >
+      ssss
     </div>
+    // API keys: aacba06b1013c772f557217737a010a6 이거 도용하면 고소할 거임
   );
 }
 
